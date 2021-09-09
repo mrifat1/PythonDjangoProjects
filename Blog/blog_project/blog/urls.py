eca6d09583728dc820aa5ca00a63027afd761e48
mrifat1 <mahmudrifat200@gmail.com>
2021-09-10 03:53:04 +0600

    commit

--- Blog/blog_project/blog/urls.py
+++ Blog/blog_project/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import blogpostview
+from .views import blogpostview,blogdetailview
 
 urlpatterns = [
+    path('post/<int:pk>/', blogdetailview.as_view(), name='post_detail'),
     path('',blogpostview.as_view(),name = 'home'),
+
+
 ]
