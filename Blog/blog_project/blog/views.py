b359e1414f46a0d9161a67793ee653e6e52304e6
mrifat1 <mahmudrifat200@gmail.com>
2021-09-10 01:26:29 +0600

    git push --help

--- Blog/blog_project/blog/views.py
+++ Blog/blog_project/blog/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import post
 
 class blogpostview(ListView):
     model = post
     template_name = 'home.html'
+
+class blogdetailview(DetailView):
+    model = post
+    template_name = 'post_detail.html'
 
 
 
